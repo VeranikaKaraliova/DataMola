1ac39886260dafe1672710555142841be0d356e3
VeranikaKaraliova <nika-11@mail.ru>
2020-12-08 14:58:05 +0300

    task 7, part 1

--- UI/scriptView.js
+++ UI/scriptView.js
@@ -5,28 +5,27 @@ class HeaderView {
 
   display(user) {
     if (!user) {
-      const loginRegistr = document.getElementById('login-registr')
-      loginRegistr.style.display = "block";
+      const loginRegistr = document.getElementById('login-registr');
+      loginRegistr.style.display = 'block';
       const removeNode = document.getElementById('send-msg');
       removeNode.style.display = 'none';
-      const activeUser = document.getElementById('authorized-user')
-        activeUser.style.display="none"
-        const circleUser = document.getElementById('circle-user')
-        circleUser.style.display="none"
-    }
-    else {
+      const activeUser = document.getElementById('authorized-user');
+      activeUser.style.display = 'none';
+      const circleUser = document.getElementById('circle-user');
+      circleUser.style.display = 'none';
+    } else {
       this.activeUser.innerHTML = `<p>${user}</p>`;
       const initials = document.getElementById('circle-user');
       initials.className = 'circle-user';
       initials.innerHTML = `<h3>${user.match(/\b(\w)/g).join('')}</h3>`;
       const removeNode = document.getElementById('send-msg');
       removeNode.style.display = 'flex';
-      const loginRegistr = document.getElementById('login-registr')
-      loginRegistr.style.display = "none";
-      const activeUser = document.getElementById('authorized-user')
-        activeUser.style.display="flex"
-        const circleUser = document.getElementById('circle-user')
-        circleUser.style.display="block"
+      const loginRegistr = document.getElementById('login-registr');
+      loginRegistr.style.display = 'none';
+      const activeUser = document.getElementById('authorized-user');
+      activeUser.style.display = 'flex';
+      const circleUser = document.getElementById('circle-user');
+      circleUser.style.display = 'block';
     }
   }
 }
@@ -35,8 +34,8 @@ class MessagesView {
     this.id = id;
   }
 
-  display(arrMsg = [], user) {
-    console.log(arrMsg)
+  display(arrMsg = [], user, skip = 0, top = 10) {
+    arrMsg = arrMsg.sort((a, b) => a.createdAt - b.createdAt).slice(skip, skip + top);
     const partMsg = document.getElementById(this.id);
     partMsg.innerHTML = '';
     for (let i = 0; i < arrMsg.length; i++) {
@@ -49,18 +48,18 @@ class MessagesView {
         <div class="sent-width-msg">
             <div class="info-msg" id="info-msg">
                 <p class="sender">${arrMsg[i].author}</p>
-                <p class="time_date">${arrMsg[i].createdAt.toLocaleString()}</p>
+                <p class="time_date">${arrMsg[i].createdAt.getHours()}:${arrMsg[i].createdAt.getMinutes()}</p>
                 <button class="possible-msg" id= "possible-msg">${'...'}</button>
             </div> 
-            <div class="change-msg">
+            <p>${arrMsg[i].text}</p>
+        </div>
+        </div>
+        <div class="change-msg" id="change-msg">
               <ul>
                 <li id='edit'>Редактировать</li>
                 <li id='delete'>Ударить</li>
               </ul>
-            </div>
-            <p>${arrMsg[i].text}</p>
-        </div>
-        </div>`;
+            </div>`;
         partMsg.appendChild(outgoingMsg);
       }
       if (arrMsg[i].author !== user) {
@@ -73,9 +72,11 @@ class MessagesView {
                                         <div class="received_withd_msg">
                                             <div class="info-msg">
                                                 <p class="sender">${arrMsg[i].author}</p>
-                                                <p class="time_date">${arrMsg[i].createdAt.toLocaleString()}</p>
+                                                <p class="time_date">${arrMsg[i].createdAt.getHours()}:${arrMsg[i].createdAt.getMinutes()}</p>
+                                                <p class="privat"></p>
                                             </div> 
                                             <p class="text-msg">${arrMsg[i].text}</p>
+                                            
                                         </div>
                                     </div>
                                   </div>`;
@@ -90,15 +91,18 @@ class ActiveUsersView {
     this.id = id;
   }
 
-  display(activeUser = []) {
+  display(activeUser = [], user) {
+    const sidebar = document.getElementById(this.id);
+    sidebar.innerHTML = '';
     for (let i = 0; i < activeUser.length; i++) {
-      const sidebar = document.getElementById(this.id);
-      const el = document.createElement('div');
-      el.className = 'user-online-container';
-      el.innerHTML = `<div class="circle-user-container"><p>${activeUser[i].match(/\b(\w)/g).join('')}</p></div> 
-                        <p class="name-user">${activeUser[i]}</p>
-                        <p class="online-user">online</p>`;
-      sidebar.append(el);
+      if (activeUser[i].isActive === true && user !== activeUser[i].name) {
+        const el = document.createElement('div');
+        el.className = 'user-online-container';
+        el.innerHTML = `<div class="circle-user-container"><p>${activeUser[i].name[0]}</p></div> 
+                          <p class="name-user">${activeUser[i].name}</p>
+                          <p class="online-user">online</p>`;
+        sidebar.append(el);
+      }
     }
   }
 }
